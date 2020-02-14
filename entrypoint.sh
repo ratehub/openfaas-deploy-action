daa05e7b579059f6e18f8e975bdfea112b817f96
brynnonpicard <brynnonpicard@gmail.com>
2020-02-14 16:30:13 -0500

    Added curly braces around option variables in the entrypoint.sh script

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,14 +26,14 @@ fi
 
 docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}" "${DOCKER_REGISTRY_URL}"
 
-if [ -n "$DOCKER_USERNAME_2" ] && [ -n "$DOCKER_PASSWORD_2" ];
+if [ -n "${DOCKER_USERNAME_2}" ] && [ -n "${DOCKER_PASSWORD_2}" ];
 then
     docker login -u "${DOCKER_USERNAME_2}" -p "${DOCKER_PASSWORD_2}" "${DOCKER_REGISTRY_URL_2}"
 fi
 
 faas-cli template pull
 
-if [ -n "$CUSTOM_TEMPLATE_URL" ];
+if [ -n "${CUSTOM_TEMPLATE_URL}" ];
 then
     faas-cli template pull "${CUSTOM_TEMPLATE_URL}"
 fi
@@ -83,7 +83,7 @@ else
                     #If we already handled this function based on a prior file, we can ignore it this time around
                     if [ "$FUNCTION_PATH" != "$FUNCTION_PATH2" ];
                     then
-                        if [ -n "$BUILD_ARG_1" ] && [ -n "$BUILD_ARG_1_NAME" ];
+                        if [ -n "${BUILD_ARG_1}" ] && [ -n "${BUILD_ARG_1_NAME}" ];
                         then
                             faas-cli build --filter="$FUNCTION_PATH" --build-arg "$BUILD_ARG_1_NAME=$BUILD_ARG_1"
                         else
