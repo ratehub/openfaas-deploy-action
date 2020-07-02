c5202a9ac944c267656c8b6665d7ae0b8842012a
rach1416 <rachitha.rajagopal@ratehub.ca>
2020-07-02 12:39:36 -0400

    add git fetch all

--- build_push/build_push.sh
+++ build_push/build_push.sh
@@ -63,7 +63,7 @@ else
     then
         git diff HEAD HEAD~1 --name-only > differences.txt
     else
-        git fetch -t
+        git fetch all
         git diff HOTFIX_TAG LATEST_TAG --stat > differences.txt
     fi
 
