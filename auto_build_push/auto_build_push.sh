a9337d6611afc4234c052a998be7a1c30df8ed18
rach1416 <rachitha.rajagopal@ratehub.ca>
2020-07-06 12:47:24 -0400

    [wip] - add back build-args for build

--- auto_build_push/auto_build_push.sh
+++ auto_build_push/auto_build_push.sh
@@ -33,8 +33,12 @@ then
     UPDATED_STACK_FILE="$(yq w "$STACK_FILE" functions.*.image "$GCR_ID""$IMAGE":"$TAG_OVERRIDE")"
     echo "$UPDATED_STACK_FILE" > $STACK_FILE
 
-    faas-cli build --build-arg "$BUILD_ARG_1_NAME=$BUILD_ARG_1" 
-
+    faas-cli build --build-arg "$BUILD_ARG_1_NAME=$BUILD_ARG_1" \
+        --build-arg "$BUILD_ARG_2_NAME=$BUILD_ARG_2_VALUE" \
+        --build-arg "$BUILD_ARG_3_NAME=$BUILD_ARG_3_VALUE" \
+        --build-arg "$BUILD_ARG_4_NAME=$BUILD_ARG_4_VALUE" \
+        --build-arg "$BUILD_ARG_5_NAME=$BUILD_ARG_5_VALUE" \
+        --build-arg "$BUILD_ARG_6_NAME=$BUILD_ARG_6_VALUE"
 
     if [ "$GITHUB_EVENT_NAME" == "push" ];
     then
