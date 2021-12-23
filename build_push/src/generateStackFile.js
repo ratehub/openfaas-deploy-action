6c131df435bbc2939c53b1cf8ed21a0b48537467
abhayvatoo <abhay.vatoo.jal@gmail.com>
2021-12-23 16:10:19 -0500

    Migrate build action

--- build_push/src/generateStackFile.js
+++ build_push/src/generateStackFile.js
@@ -1,84 +1,46 @@
 const core = require('@actions/core');
 
 const { dump, safeLoad } = require('js-yaml');
-const { readFileSync, writeFileSync, existsSync } = require('fs');
-const { isArray, mergeWith, uniq } = require('lodash');
+const { readFileSync, writeFileSync } = require('fs');
 
 
-async function generateStackFile(groupPath, subPath, environment) {
-    const configOverride = core.getInput('config-override');
+async function generateStackFile(tag, gcrProjectId) {
     const stackFile = core.getInput('stack-file');
-    const gcrProjectId = core.getInput('image-registry');
-    const tagOverride = core.getInput('tag-override');
-
-    const stackFilePath = `${groupPath}/${stackFile}`;
-    const globalSettingsFilePath = `${groupPath}/global-${environment}-deploy.yml`;
-    const localSettingsFilePath = configOverride === 'none'
-        ? `${groupPath}/${subPath}/${environment}-deploy.yml`
-        : `${groupPath}/${subPath}/${configOverride}`;
 
     try {
-        // read all yamls
-        const stack = safeLoad(readFileSync(stackFilePath, 'utf8'));
-        const globalSettings = existsSync(globalSettingsFilePath) ? safeLoad(readFileSync(globalSettingsFilePath, 'utf8')) : {};
-        const deployFunctions = safeLoad(readFileSync(localSettingsFilePath, 'utf8'));
+        // read stack
+        const stack = safeLoad(readFileSync(stackFile, 'utf8'));
 
-        // 1.0 is converted to 1 while converting yaml to json
+        // version 1.0 is converted to 1 while converting yaml to json
         stack.version = stack.version.toFixed(1);
         const functions = stack.functions;
 
-        // merge global settings
-        const functionsWithGlobalSettings = Object.keys(functions).reduce((acc, key) => {
-            acc[key] = mergeWith({}, functions[key], globalSettings, customizer);
-            return acc;
-        }, {});
-
-        // merge function specific settings
-        const updatedFunctions = deployFunctions
-            ? Object.keys(deployFunctions).reduce((acc, key) => {
-                const globalSettingFunction = functionsWithGlobalSettings[key]
-                    ? functionsWithGlobalSettings[key]
-                    : functionsWithGlobalSettings[Object.keys(functionsWithGlobalSettings)[0]];
-
-                acc[key] = mergeWith({}, globalSettingFunction, deployFunctions[key], customizer);
-
-                return acc;
-            }, {})
-            : functionsWithGlobalSettings;
-
-        // append GCR project ID to image apply override tag
-        Object.keys(updatedFunctions).forEach(key => {
-            const image = updatedFunctions[key].image;
-            // read the tag from image after `:`
-            const tag = image.match(/:(.*)/g).pop().replace(":", "");
-            const imageWithUpdatedTag = tagOverride
-                ? image.replace(tag, tagOverride)
+        // Append GCR project ID to image and tag
+        Object.keys(functions).forEach(key => {
+            const image = functions[key].image;
+            // read the tag's value from image after `:`
+            const stackTag = image.match(/:(.*)/g).pop().replace(":", "");
+            const imageWithUpdatedTag = tag
+                ? image.replace(stackTag, tag)
                 : image;
-
             const imageWithProjectId = `${gcrProjectId}${imageWithUpdatedTag}`;
-            updatedFunctions[key].image = imageWithProjectId;
+            functions[key].image = imageWithProjectId;
         });
 
-        // create final stack json
+        // Create updated/final stack json
         const updatedStack = {
             ...stack,
-            functions: updatedFunctions
+            functions: functions
         }
 
-        // write final stack to updated-stack.yml
+        // Write updated/final stack.yml - `updated-stack.yml`
         writeFileSync('updated-stack.yml', dump(updatedStack, {
-            // do not convert duplicate objects into references
+            // Don't convert duplicate objects into references
             noRefs: true
         }));
     } catch (e) {
         console.log('Error generating final stack file:', e);
         process.exit(1);
-    }
-}
-
-function customizer(objValue, srcValue) {
-    if (isArray(objValue)) {
-        return uniq(objValue.concat(srcValue));
     }
 }
 
--- deploy/deploy.js
+++ deploy/deploy.js
@@ -6,25 +6,28 @@ const {
     generateStackFile,
 } = require('./src');
 
+
+const FAAS = `${process.env.GITHUB_WORKSPACE}/faas-cli`;
+
 (async () => {
     try {
-        await installFaasCli();
+        await installFaasCli({ isLoginRequired: true });
 
         const groupPath = core.getInput('group-path');
         const subPath = core.getInput('deploy-function');
         const environment = core.getInput('deployment-env');
-        console.log(`>>> Generating stack file: ${groupPath}/${subPath}`);
+        console.log(`Generating stack file: ${groupPath}/${subPath}`);
 
         await generateStackFile(groupPath, subPath, environment);
 
         // do not log FE exporter stack
         if (!groupPath.includes('frontend-export')) {
-            console.log('>>> Generated stack file:');
+            console.log('Generated stack file:');
             await exec.exec('cat updated-stack.yml');
         }
 
         const gateway = core.getInput('openfaas-gateway');
-        await exec.exec(`${process.env.GITHUB_WORKSPACE}/faas-cli deploy -f updated-stack.yml --gateway=${gateway}`);
+        await exec.exec(`${FAAS} deploy -f updated-stack.yml --gateway=${gateway}`);
     } catch (error) {
         core.setFailed(error.message);
     }
